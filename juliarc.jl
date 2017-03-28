f53b99471b03c209edfa830e455e8534dfb0fd3f
Julius Donnert <donnert@ira.inaf.it>
2017-03-28 13:56:10 -0500

    Load StringConversoin

--- juliarc.jl
+++ juliarc.jl
@@ -1,4 +1,4 @@
-# Julia startup file
+# Julia startup file - all line are executed on the startup task
 
 @everywhere push!(LOAD_PATH, "/Users/jdonnert/Dev/src/git/Tandav/lib/julia/")
 @everywhere push!(LOAD_PATH, "/Users/jdonnert/Dev/src/git/julia/")
@@ -6,32 +6,30 @@
 
 #using ClobberingReload
 
-using CGSUnits 	# Physical constants and conversion
+using CGSUnits 			# Physical constants and conversion
+using StringConversion	# convert integer to strings with zero padding
 using ArrayStatistics
 using Binning
 using Cosmology
 
 # PGF Plotting
-## using ColorPGF
-### InitPGF()
+using ColorPGF
+InitPGF()
 
 # Plots interface, pyplot, modify standard appearance 
 import ColorBrewer
 colorBrewerSet1 = ColorBrewer.palette("Set1", 9)
 
 using Plots
-
-pyplot(size=(640,480), grid=false, legend=:none, linewidth=1, markersize=1,
-	   left_margin=[5mm 15mm], bottom_margin=[5mm 15mm], 
-	   color_palette=colorBrewerSet1) 
+gr(size=(512, 512/sqrt(2)), grid=false, legend=:none, linewidth=1, markersize=1,
+	 margin=5mm, color_palette=colorBrewerSet1, xlabel="x", ylabel="y",
+ 	 markerstrokewidth=0, tickfont=font(7, "Helvetica"), grid=true ) 
 
 
 # Gadget/Tandav reading
 
 using Tandav
-tandav = TandavCodeObject()
 
 # Wombat I/O
 
 #importall Wombat
-#wb = WombatCodeObject()
